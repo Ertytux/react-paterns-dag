80b9aae0fda5d7d5b4245e78e240b35782e9fcfb
Edisel Navas Conyedo <enavas@uci.cu>
2024-02-21 23:43:23 -0500

    Work on node structures

--- src/components/dagEditor.tsx
+++ src/components/dagEditor.tsx
@@ -1,36 +1,53 @@
 import React, { useState } from 'react';
 
-// Definimos el tipo para los nodos del DAG
-interface DagNode {
-  id: string;
-  label: string;
-  // Puedes agregar más propiedades según tus necesidades
+//Tipos de nodo
+enum TypeNode{
+  InputNode,
+  ProcessNode,
+  OutputNode
 }
 
-// Componente DAG
+// Definición de nodo 
+interface DagNode {
+  id: number;
+  label: string;
+  typeNode:TypeNode;
+  description:string;
+  inputNodes:DagNode[];
+  value:number;
+  evaluator():number;  
+}
+
+// Componente 
 const DagComponent: React.FC = () => {
-  // Estado para almacenar los nodos del DAG
+  // Estado para almacenar los nodos 
   const [nodes, setNodes] = useState<DagNode[]>([]);
 
-  // Función para agregar un nuevo nodo al DAG
+  // Función para agregar un nuevo nodo 
   const addNode = (newNode: DagNode) => {
     setNodes([...nodes, newNode]);
   };
 
+  //Funci'on para eliminar un nodo
+  const removeNode = (oldNode:DagNode)=> {
+    let index=nodes.findIndex(x=>x===oldNode);
+    setNodes(nodes.splice(index));
+  };
+
   // Renderizamos los nodos
-  const renderNodes = () => {
+  function renderNodes() {
     return nodes.map((node) => (
       <div key={node.id}>
         {node.label}
         {/* Puedes personalizar la representación visual de cada nodo */}
       </div>
     ));
-  };
+  }
 
   return (
     <div>
       <h2>Componente DAG</h2>
-      <button onClick={() => addNode({ id: '1', label: 'Nodo 1' })}>
+      <button>
         Agregar Nodo
       </button>
       <div>{renderNodes()}</div>
